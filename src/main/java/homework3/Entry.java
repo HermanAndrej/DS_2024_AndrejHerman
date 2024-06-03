ac2ff50660874ce10b71bed0aaef745833336a58
HermanAndrej <hermanandrej03@gmail.com>
2024-06-03 20:06:11 +0200

    Homework 3 finished

---
+++ src/main/java/homework3/Entry.java
@@ -0,0 +1,38 @@
+package homework3;
+
+public class Entry implements Comparable<Entry> {
+    private String name;
+    private String streetAddress;
+    private String city;
+    private String postcode;
+    private String country;
+    private String phoneNumber;
+
+    public Entry(String name, String streetAddress, String city, String postcode, String country, String phoneNumber) {
+        this.name = name;
+        this.streetAddress = streetAddress;
+        this.city = city;
+        this.postcode = postcode;
+        this.country = country;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int compareTo(Entry other) {
+        return this.name.compareTo(other.name);
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + name + "\n" +
+                "Street address: " + streetAddress + "\n" +
+                "City: " + city + "\n" +
+                "Postal code: " + postcode + "\n" +
+                "Country: " + country + "\n" +
+                "Phone number: " + phoneNumber + "\n";
+    }
+}
